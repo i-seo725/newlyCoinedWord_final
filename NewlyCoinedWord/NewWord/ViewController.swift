015019bc0ebacdea38c6db0ff343681d641535e7
i-seo725 <hjing719@gmail.com>
2023-07-25 18:11:33 +0900

    코드 복사 붙여넣기 했는데 인덱스 에러...

--- NewlyCoinedWord/NewWord/ViewController.swift
+++ NewlyCoinedWord/NewWord/ViewController.swift
@@ -8,8 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var wordButtons: [UIButton]!
+    @IBOutlet var firstButton: UIButton!
+    @IBOutlet var secondButton: UIButton!
+    @IBOutlet var thirdButton: UIButton!
+    @IBOutlet var fourthButton: UIButton!
+    
     @IBOutlet var resultLabel: UILabel!
     @IBOutlet var searchButton: UIButton!
     @IBOutlet var searchTextField: UITextField!
@@ -17,12 +22,14 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        resultImageView.image = UIImage(named: "word_logo")
         designResultLabel()
         designButton()
         designTextField()
         designSearchButton()
-        resultImageView.image = UIImage(named: "word_logo")
-        makeRandomButton()
+        //        makeRandomButton()
+        wordExampleTapped(wordButtons)
+        //        firstButton.isHidden = true
     }
     
     func designResultLabel() {
@@ -38,7 +45,7 @@ class ViewController: UIViewController {
             item.configuration?.titleAlignment = .center
             item.layer.borderWidth = 1
             item.layer.borderColor = UIColor.black.cgColor
-
+            
         }
     }
     func designTextField() {
@@ -54,41 +61,85 @@ class ViewController: UIViewController {
         searchButton.tintColor = .white
     }
     
+    
+    func makeAlert(){
+        let alert = UIAlertController(title: "입력값 오류", message: "두 글자 이상 입력해주세요.", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "확인", style: .cancel)
+        alert.addAction(ok)
+        present(alert, animated: true)
+    }
+    
     var newWord = ["군싹": "군침이 싹 도네", "남아공": "남아서 공부나 해라", "돈쭐": "착한 일을 한 가게에 찾아가서 물건이나 음식을 팔아주는 행위", "돼지런하다": "평소에는 게으른데 먹을 때만 부지런하다", "런치플레이션": "점심값이 너무 비싸다", "무물보": "무엇이든 물어보세요", "어쩔티비": "어쩌라고", "sbn": "선배님", "ㄹㄱㅎㅃ": "라고할뻔의 초성어로 약올릴 때 쓰는 말", "내또출": "내일 또 출근", "편리미엄": "편리한 것이 프리미엄이다", "쩝쩝박사": "음식의 조합을 잘 맞추는 사람", "인급동": "인기 급상승 동영상의 약자로 유튜브에서 많이 사용하는 단어", "드르륵 탁": "테이프를 되감기 할 떄 나는 소리로 다시 보고 싶은 장면에 사용하는 신조어"]
     
-    func makeRandomButton() {
-        var words: Set<String> = []
-        while words.count != 4 {
-            words.insert(newWord.randomElement()!.key)
-        }
-        var count = 0
+    //    func makeRandomButton() {
+    //        var words: Set<String> = []
+    //        while words.count != 4 {
+    //            words.insert(newWord.randomElement()!.key)
+    //        }
+    //        var count = 0
+    //        for i in wordButtons {
+    //            i.setTitle(Array(words)[count], for: .normal)
+    //            count += 1
+    //        }
+    //
+    //    }
+    
+    @IBAction func wordExampleTapped(_ sender: [UIButton]) {
+        let shuffledWords = newWord.keys.shuffled()
         for i in wordButtons {
-            i.setTitle(Array(words)[count], for: .normal)
-            count += 1
+            i.setTitle(shuffledWords[i.tag - 1], for: .normal)
         }
-        
     }
+    
     
     @IBAction func searchButtonTapped(_ sender: UIButton) {
         searchTextField.text = sender.currentTitle
         typeWordTextField(searchTextField)
-        makeRandomButton()
+        //        makeRandomButton()
+        wordExampleTapped(wordButtons)
     }
     
-    @IBAction func typeWordTextField(_ sender: Any) {
+    func searchAction() {
         resultImageView.image = UIImage(named: "background")
-        for i in newWord.keys {
-            if searchTextField.text == i {
-                resultLabel.text = "\"\(i)\"의 뜻은 \n\"\(newWord[i]!)\"입니다."
-                break
-            } else {
-                resultLabel.text = "입력하신 \"\(searchTextField.text ?? "")\" 단어는 사전에 존재하지 않습니다."
+            for i in newWord.keys {
+                if searchTextField.text! == i {
+                    resultLabel.text = "\"\(i)\"의 뜻은 \n\"\(newWord[i]!)\"입니다."
+                    break
+                } else {
+                    resultLabel.text = "입력하신 단어는 \n 사전에 존재하지 않습니다."
+                }
             }
-        }
-        makeRandomButton()
+        wordExampleTapped(wordButtons)
         view.endEditing(true)
     }
     
+//    @IBAction func searchButtonClicked(_ sender: UIButton) {
+//        let typedTextCount = searchTextField.text
+//        guard let typedCount = searchTextField.text?.count else {
+//                makeAlert()
+//                return
+//        }
+//        if typedCount >= 2 {
+//            for i in newWord.keys {
+//                if searchTextField.text! == i {
+//                    resultLabel.text = "\"\(i)\"의 뜻은 \n\"\(newWord[i]!)\"입니다."
+//                    break
+//                } else {
+//                    resultLabel.text = "입력하신 단어는 \n 사전에 존재하지 않습니다."
+//                }
+//            }
+//
+//
+//        } else {
+//        makeAlert()
+//            return
+//        }
+//    }
+    
+    @IBAction func typeWordTextField(_ sender: Any) {
+        
+        searchAction()
+    }
     
     @IBAction func viewTappedGesture(_ sender: UITapGestureRecognizer) {
         view.endEditing(true)
